e60bb99160e7c9d5827661cb923131fe062003e4
micoconnell <105730018+micoconnell@users.noreply.github.com>
2023-01-26 08:34:51 -0700

    Add files via upload

---
+++ recv.py
@@ -0,0 +1,26 @@
+import asyncio
+from azure.servicebus.aio import ServiceBusClient
+import os
+
+
+NAMESPACE_CONNECTION_STR =''
+QUEUE_NAME = 'servicebus'
+print("Working")
+async def run():
+    # create a Service Bus client using the connection string
+    async with ServiceBusClient.from_connection_string(
+        conn_str=NAMESPACE_CONNECTION_STR,
+        logging_enable=True) as servicebus_client:
+        print("Working")
+        async with servicebus_client:
+            # get the Queue Receiver object for the queue
+            receiver = servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME)
+            async with receiver:
+                received_msgs = await receiver.receive_messages(max_wait_time=500, max_message_count=100)
+                for msg in received_msgs:
+                    print("Received: " + str(msg))
+                    # complete the message so that the message is removed from the queue
+                    await receiver.complete_message(msg)
+                    
+                    
+asyncio.run(run())
